549f95f30ec7057b12b2933a988cd6f34b9d0448
Wabram <immunol22@gmail.com>
2018-11-04 13:59:53 +0100

    R code

--- plot1.R
+++ plot1.R
@@ -4,15 +4,20 @@ hpc <- read.csv("H:/Projects/Courses/R/EDA/project_1/household_power_consumption
 ## data interesting for EDA project 1 
 hpc<-hpc[hpc$Date=="1/2/2007" | hpc$Date=="2/2/2007",]
 ## write hpc object as a txt file in your working directory for speed up future loading interesting data for EDA project 1
-write.table(hpc,file="hpc.txt")
+write.table(hpc,file="hpc.txt") # will change factor to numeric in columns with numeric variables
+## change structure of third column hpc$Global_active_power from factor to numeric
+hpc$Global_active_power<-as.numeric(paste(hpc$Global_active_power))
+
 
 # or
 ## read hpc.txt file according to the below code
 hpc <- read.csv("H:/Projects/Courses/R/EDA/ExploratoryDataAnalysis/hpc.txt", sep="")
 ## display structure of hpc object
 str(hpc)
+
 ## generate histogran in local graphic device
 hist(hpc$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+
 ## create histogran from local graphic device to the file with png format
 dev.copy(png,file="plot1.png",width=480,height=480)
 dev.off()
